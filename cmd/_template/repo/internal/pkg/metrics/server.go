7a2bcf16ba548d693f657008d107671b0adf26b7
Adam Reynolds <adam.reynolds@fresh8gaming.com>
2022-06-30 11:18:21 +0100

    ensure port is passed to function

--- cmd/_template/repo/internal/pkg/metrics/server.go
+++ cmd/_template/repo/internal/pkg/metrics/server.go
@@ -9,18 +9,18 @@ import (
 	"go.uber.org/zap"
 )
 
-func StartServer() {
+func StartServer(port string) {
 	logger := logging.GetLogger()
 
 	metricsServer := &http.Server{
-		Addr:         fmt.Sprintf("0.0.0.0:%s", config.Get().MPort),
+		Addr:         fmt.Sprintf("0.0.0.0:%s", port),
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
 		IdleTimeout:  time.Second * 60,
 		Handler:      GetRouter(),
 	}
 
-	logger.Info("starting metrics on 9898", zap.String(config.Get().MPort))
+	logger.Info("starting metrics on 9898", zap.String("port",port))
 
 	go func() {
 		if err := metricsServer.ListenAndServe(); err != nil {
