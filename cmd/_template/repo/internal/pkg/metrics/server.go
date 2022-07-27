c0e18ec0a7766042e1a5dea37f767addb31bb12c
Adam Reynolds <adam.reynolds@fresh8gaming.com>
2022-07-27 16:43:32 +0100

    ensure ReadHeaderTimeout is set

--- cmd/_template/repo/internal/pkg/metrics/server.go
+++ cmd/_template/repo/internal/pkg/metrics/server.go
@@ -14,11 +14,12 @@ func StartServer(port string) {
 	logger := logging.GetLogger()
 
 	metricsServer := &http.Server{
-		Addr:         fmt.Sprintf("0.0.0.0:%s", port),
-		WriteTimeout: time.Second * 5,
-		ReadTimeout:  time.Second * 5,
-		IdleTimeout:  time.Second * 60,
-		Handler:      GetRouter(),
+		Addr:              fmt.Sprintf("0.0.0.0:%s", port),
+		WriteTimeout:      time.Second * 5,
+		ReadTimeout:       time.Second * 5,
+		IdleTimeout:       time.Second * 60,
+		ReadHeaderTimeout: time.Second * 2,
+		Handler:           GetRouter(),
 	}
 
 	logger.Info("starting metrics on 9898", zap.String("port",port))
