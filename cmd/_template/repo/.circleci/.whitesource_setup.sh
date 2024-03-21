1b1bcbd19fc3cdb302aeb9ef2b2667fbdda153fe
Dom Udall <d.udall@sportradar.com>
2024-03-21 17:11:22 +0000

    Switch from CircleCI to Gitlab CI

--- cmd/_template/repo/.circleci/.whitesource_setup.sh
+++
@@ -1,4 +0,0 @@
-curl -L https://go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz --output go.tar.gz
-tar -C /usr/local -xzf go.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> $BASH_ENV
-echo "export WS_PROJECTNAME=$CIRCLE_PROJECT_REPONAME" >> $BASH_ENV
